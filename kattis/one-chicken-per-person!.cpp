987034cfa807ad100fc7adf4828fefce79d8a4c0
tree_hugger <155770085+marc3lllob7@users.noreply.github.com>
2026-08-05 23:20:39 -0500

    feat(kattis): add one chicken per person! problem

---
+++ kattis/one-chicken-per-person!.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int n, m;
+	cin >> n >> m;
+	if (m > n){
+		if (m - n == 1) cout << "Dr. Chaz will have 1 piece of chicken left over!";
+		else cout << "Dr. Chaz will have " << m - n << " pieces of chicken left over!";
+	}
+	else {
+		if (n - m == 1) cout << "Dr. Chaz needs 1 more piece of chicken!";
+		else cout << "Dr. Chaz needs " << n - m << " more pieces of chicken!";
+	}
+}
