a52bc383b753b9431889739b121c34cb3cbcfc1d
tree_hugger <155770085+marc3lllob7@users.noreply.github.com>
2026-08-06 00:45:44 -0500

    feat(kattis): add quadrant-selection problem

---
+++ kattis/quadrant-selection.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int x, y; cin >> x >> y;
+	if (x > 0 && y > 0) cout << 1;
+	if (x > 0 && y < 0) cout << 4;
+	if (x < 0 && y > 0) cout << 2;
+	if (x < 0 && y < 0) cout << 3;
+}
