92dfcf7c7f118e5f1986d0493acbe7c91a9d7f23
tree_hugger <155770085+marc3lllob7@users.noreply.github.com>
2026-08-06 20:58:47 -0500

    feat(kattis): add army strength (easy) problem v2

---
+++ kattis/army-strength-easy-v2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long t, gN, mN;
+    cin >> t;
+    while(t--){
+        long long idx, maxG = -1, maxM = -1;
+        cin >> gN >> mN;
+        for(int i = 0; i < gN; i++){
+            cin >> idx;
+            maxG = max(maxG, idx);
+        }
+        for(int i = 0; i < mN; i++){
+            cin >> idx;
+            maxM = max(maxM, idx);
+        }
+        if(maxG >= maxM) cout << "Godzilla\n";
+        else cout << "MechaGodzilla\n";
+    }
+}
