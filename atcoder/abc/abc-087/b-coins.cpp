a2ddffebc8440650b4e7ca581b71411852034975
tree_hugger <155770085+marc3lllob7@users.noreply.github.com>
2026-08-10 09:48:43 -0500

    feat(atcoder.abc.abc-087): add b problem: coins

---
+++ atcoder/abc/abc-087/b-coins.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b, c, x, ans = 0;
+    cin >> a >> b >> c >> x;
+    for (int i = 0; i <= a; i++)
+        for (int j = 0; j <= b; j++)
+            for (int k = 0; k <= c; k++)
+                if ((i * 500) + (j * 100) + (k * 50) == x) ans++;
+    cout << ans;
+}
