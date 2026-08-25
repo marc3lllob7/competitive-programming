778aceff765337895786ccb03bb39d8936fbbc3f
tree_hugger <155770085+marc3lllob7@users.noreply.github.com>
2026-08-24 19:58:09 -0500

    feat(cses.01-introductory-problems): add weird algorithm problem

---
+++ cses/01-introductory-problems/weird-algorithm.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	long long n;
+    cin >> n;
+    cout << n << ' ';
+    while(n != 1){
+        if(n % 2) n *= 3, n++;
+        else n /= 2;
+        cout << n << ' ';
+    }
+}
