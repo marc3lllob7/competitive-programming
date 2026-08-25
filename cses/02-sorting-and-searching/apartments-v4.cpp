39ef00362a2ee8480ab1cf413e47571f81228834
tree_hugger <155770085+marc3lllob7@users.noreply.github.com>
2026-08-24 21:15:03 -0500

    feat(cses.02-sorting-and-searching): add apartments v4 problem

---
+++ cses/02-sorting-and-searching/apartments-v4.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m, k;
+    int cont, i, j;
+    cont = 0;
+    cin >> n >> m >> k;
+    vector <int> a(n), b(m);
+    for (i = 0; i < n; i++)
+        cin >> a[i];
+    for (i = 0; i < m; i++)
+        cin >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end()); 
+    i = j = 0;
+    while (i < n && j < m){
+        if (abs(a[i] - b[j]) <= k){
+            cont++;
+            i++;
+            j++;
+	}
+	else if(a[i] - b[j] > k) j++;
+        else i++;
+     }
+    cout << cont;
+    return 0;
+}
