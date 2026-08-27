9d7eb55fb9116732ac827765f3318b8c21538cab
tree_hugger <155770085+marc3lllob7@users.noreply.github.com>
2026-08-27 14:06:47 -0500

    feat(cses.01-introductory-problems): add trailing zeros problem

---
+++ cses/01-introductory-problems/trailing-zeros.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n, ans = 0;
+    cin >> n;
+    while(n > 0){
+        n /= 5;
+        ans += n;
+    }
+    cout << ans;
+}
